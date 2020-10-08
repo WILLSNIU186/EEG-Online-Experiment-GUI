9eeeb9453ae72d893eb11bdb44d22a7c7e6f72dc
Jin <willsnjs@outlook.com>
2020-10-07 22:09:36 -0400

    remove venv

--- venv/Scripts/miniterm.py
+++
@@ -1,4 +0,0 @@
-#!C:\uw_ebionics_mrcp_online_interface_python\venv\Scripts\python.exe
-# EASY-INSTALL-SCRIPT: 'pyserial==3.4','miniterm.py'
-__requires__ = 'pyserial==3.4'
-__import__('pkg_resources').run_script('pyserial==3.4', 'miniterm.py')
--- venv/Scripts/pywin32_postinstall.py
+++
@@ -1,4 +0,0 @@
-#!C:\uw_ebionics_mrcp_online_interface_python\venv\Scripts\python.exe
-# EASY-INSTALL-SCRIPT: 'pywin32==227','pywin32_postinstall.py'
-__requires__ = 'pywin32==227'
-__import__('pkg_resources').run_script('pywin32==227', 'pywin32_postinstall.py')
