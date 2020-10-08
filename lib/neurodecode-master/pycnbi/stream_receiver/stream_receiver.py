0ac48a77fcf02a43c4a4c6c9a8899a2316b8376d
Jin <willsnjs@outlook.com>
2020-10-07 22:07:19 -0400

    modify streamReceiver

--- lib/neurodecode-master/pycnbi/stream_receiver/stream_receiver.py
+++ lib/neurodecode-master/pycnbi/stream_receiver/stream_receiver.py
@@ -173,8 +173,8 @@ class StreamReceiver:
                         logger.info('Found an NE server %s (type %s, amp_serial %s) @ %s.' % (amp_name, si.type(), amp_serial, si.hostname()))
                         # ch_list = pu.lsl_channel_list(inlet)
                         ch_list = ['P7', 'P4', 'Cz', 'Pz', 'P3', 'P8', 'O1', 'O2', 'T8', 'F8', 'C4', 'F4', 'Fp2', 'Fz',
-                                   'C3', 'F3', 'Fp1', 'T7', 'F7', 'Oz', 'PO4', 'FC6', 'FC2', 'AF4', 'CP6', 'CP2', 'CP1',
-                                   'CP5', 'FC1', 'FC5', 'AF3', 'PO3']
+                                   'C3', 'F3', 'Fp1', 'T7', 'F7', 'Fpz', 'C2', 'FC6', 'FC2', 'AF4', 'CP6', 'CP2', 'CP1',
+                                   'CP5', 'FC1', 'FC5', 'AF3', 'C1']
                         # self._lsl_tr_channel = find_event_channel(ch_names=ch_list)
                         channels = len(ch_list)
                         amps.append(si)
